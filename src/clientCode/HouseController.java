579123bb85bbd5dee591ad4a0a77fa28e688cc5f
davekolian <davekolian@users.noreply.github.com>
2020-10-01 19:34:52 +0200

    Addition of fxml file
    Co-Authored-By: Ali5hadman <68865941+Ali5hadman@users.noreply.github.com>

---
+++ src/clientCode/HouseController.java
@@ -0,0 +1,13 @@
+package clientCode;
+
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HouseController implements Initializable {
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
